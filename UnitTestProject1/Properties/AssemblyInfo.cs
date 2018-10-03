b6f78016779e9c251e320eca8366b84fc1615be2
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2018-10-03 09:39:27 +0200

    unittest tilføjet

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca633d7e-c7db-4005-a61b-4f4f1827f9c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        const double delta = 0.00001;
+
+        [TestMethod]
+        public void TwoPropertiesUsesOneVariable01()
+        {
+            Temperature temperature = new Temperature();
+            temperature.Celsius = 50.0;
+
+            Assert.AreEqual(50, temperature.Celsius, delta);
+            Assert.AreEqual(122.0, temperature.Fahrenheit, delta);
+        }
+
+        [TestMethod]
+        public void TwoPropertiesUsesOneVariable02()
+        {
+            Temperature temperature = new Temperature();
+            temperature.Fahrenheit = 55.5;
+
+            Assert.AreEqual(55.5, temperature.Fahrenheit, delta);
+            Assert.AreEqual(13.05555, temperature.Celsius, delta);
+        }
+
+        [TestMethod]
+        public void TwoPropertiesUsesOneVariable03()
+        {
+            Temperature temperature = new Temperature();
+
+            Assert.AreEqual(0.0, temperature.Celsius, delta);
+            Assert.AreEqual(32.0, temperature.Fahrenheit, delta);
+        }
+
+    }
+}
